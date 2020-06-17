ea0d260d9b3bb8f8558f12ae46cfd2df14f6446f
Kathryn Dawdy <dawdy1@kenyon.edu>
2020-06-17 15:32:49 -0500

    Updated + added code to MappingLiverwortDiversity.R

--- Scripts/MappingLiverwortDiversity.R
+++ Scripts/MappingLiverwortDiversity.R
@@ -1,18 +1,35 @@
 #MAPPING LIVERWORT BETA DIVERSITY
-#Adapted from MappingDiversity.R
+#Adapted from MappingDiversity.R and Bryophytes.Rmd
 #Kathryn Dawdy, Summer 2020
 
-#Load blank raster and cell richness data + extract cell IDs and create vector for all cells
-#Change file depending on if you want to map bryophytes, mosses, liverworts, etc. 
+#Load Packages
+require(knitr)
+require(latexpdf)
+require(sp)
+require(vegan)
+require(raster)
+require(rasterVis)
+require(wesanderson)
+require(ggplot2)
+require(gridExtra)
+require(sf)
+require(rgdal)
 
+# 0.0 Run DataProcessing.R and LiverwortDiversity.R to generate necessary data -----------------------------------------------------------------
+
+## Load blank raster and cell richness data 
+##Change file depending on if you want to map bryophytes, mosses, liverworts, etc. 
 BlankRas <-raster("Data/blank_100km_raster.tif")
-LiverwortBetaMat <- readRDS("Data/LiverwortBetaMat.rds")
-LiverwortRichness <- readRDS("Data/LiverwortRichness.rds")
-CellID <- LiverwortRichness$CellID
+BetaMat <- readRDS("Data/LiverwortBetaMat.rds")
+CellRichness <- readRDS("Data/LiverwortRichness.rds")
+
+
+# 1.0 Extract cell IDs and create vector for all cells -----------------------------------------------------------------------------------------
+CellID <- CellRichness$CellID
 CellVec <- c(1:15038)
 
-#Identify occupied cells that are adjacent to each occuppied cell + convert to vector
 
+# 1.1 Identify occupied cells that are adjacent to each occuppied cell + convert to vector
 neighbor <- function(CellVec) {(adjacent(BlankRas, CellVec, directions=8, pairs=FALSE, target=CellID, sorted=TRUE, include=FALSE, id=FALSE))}
 Neighbors <- lapply(CellVec, neighbor)
 names(Neighbors) <- CellVec
@@ -21,9 +38,7 @@ bryneighbors <- Neighbors[CellID]
 bryneighborvect <- unlist(lapply(bryneighbors, length))
 
 
-
-#Separate out occuppied cells with 8 and 7 occuppied neighbors
-
+# 1.2 Separate out occuppied cells with 8 and 7 occuppied neighbors
 Cell8 <- CellID[which(bryneighborvect==8)]
 Neighbors8 <-Neighbors[Cell8]
 Neighbors8 <- data.frame(Neighbors8)
@@ -35,41 +50,33 @@ Neighbors7 <- data.frame(Neighbors7)
 names(Neighbors7) <- Cell7
 
 
-
-#Make beta diversity matrix for all cells
-
-LiverwortBetaMat<-as.matrix(LiverwortBetaMat)
-row.names(LiverwortBetaMat) <- CellID
-names(LiverwortBetaMat) <- CellID
+# 1.3 Make beta diversity matrix for all cells
+BetaMat<-as.matrix(BetaMat)
+row.names(BetaMat) <- CellID
+names(BetaMat) <- CellID
 
 
+# 1.4 Make beta diversity matrix for cells with 8 neighbors and cells with 7 neighbors
+BetaMat8<- BetaMat[!Cell8, !Cell8, drop=TRUE]
+inx8 <- match(as.character(Cell8), rownames(BetaMat8))
+BetaMat8 <- BetaMat8[inx8,inx8]
 
-#Make beta diversity matrix for cells with 8 neighbors and cells with 7 neighbors
-
-LiverwortBetaMat8<- LiverwortBetaMat[!Cell8, !Cell8, drop=TRUE]
-inx8 <- match(as.character(Cell8), rownames(LiverwortBetaMat8))
-LiverwortBetaMat8 <- LiverwortBetaMat8[inx8,inx8]
-
-LiverwortBetaMat7 <- LiverwortBetaMat[!Cell7, !Cell7, drop=TRUE]
-inx7 <- match(as.character(Cell7), rownames(LiverwortBetaMat7))
-LiverwortBetaMat7 <- LiverwortBetaMat7[inx7,inx7]
+BetaMat7 <- BetaMat[!Cell7, !Cell7, drop=TRUE]
+inx7 <- match(as.character(Cell7), rownames(BetaMat7))
+BetaMat7 <- BetaMat7[inx7,inx7]
 
 
-
-#For each cell, pairwise beta diversity is calculated for that focal cell and each of its 8 (or 7) neighbors, and the mean of those values is found
-
+# 1.5 For each cell, pairwise beta diversity is calculated for that focal cell and each of its 8 (or 7) neighbors, and the mean of those values is found
 Cell8CH <- as.character(Cell8)
-Beta8 <- lapply(Cell8CH, function(x)mean(LiverwortBetaMat[x, as.character(Neighbors8[,x])]))
+Beta8 <- lapply(Cell8CH, function(x)mean(BetaMat[x, as.character(Neighbors8[,x])]))
 names(Beta8) <- Cell8CH
 
 Cell7CH <- as.character(Cell7)
-Beta7 <- lapply(Cell7CH, function(x)mean(LiverwortBetaMat[x, as.character(Neighbors7[,x])]))
+Beta7 <- lapply(Cell7CH, function(x)mean(BetaMat[x, as.character(Neighbors7[,x])]))
 names(Beta7) <- Cell7CH
 
 
-
-#Plot mean pairwise beta diversity by Cell ID
-
+# 1.6 Plot mean pairwise beta diversity by Cell ID and convert to dissimilarity using 1-x where x is Jaccard similarity
 Beta7Vec<-unlist(Beta7)
 Beta8Vec<-unlist(Beta8)
 
@@ -79,43 +86,73 @@ BetaVec[Cell8]<-Beta8Vec
 BetaVec[Cell7]<-Beta7Vec
 
 BetaVec[BetaVec==0]<-NA
-
+BetaVec <- 1-BetaVec
 
 plot(BetaVec, ylab = "Mean Pairwise β-Diversity", xlab = "Cell ID")
 
 
-#Map beta diversity
-require(wesanderson)
-require(ggplot2)
+# 2.0 Map liverwort beta diversity -------------------------------------------------------------------------------------------------------------
 
-cols <- rev(wes_palette("Zissou1", 500, type = "continuous"))
+# 2.1 Create colorscheme
+cols <- (wes_palette("Zissou1", 500, type = "continuous"))
 
-#BetaVec[BetaVec<0.5]<-NA
-BetaRaster <- setValues(BlankRas, BetaVec)
 
-theme_set(theme_void())
-
-#Mapping method from MappingDiversity.R
-RawLivBetaMapRas <- gplot(BetaRaster, maxpixels=15038) + geom_raster(aes(fill = value))+ scale_fill_gradientn(name="β-diversity", colours=cols, na.value="transparent", limits=c(0,1)) +
-  coord_equal() 
-RawLivBetaMapRas
-
-#Mapping method from Bryophytes.Rmd
-BetaRaster <- setValues(BlankRas, BetaVec)
-BetaPoints<-rasterToPoints(BetaRaster)
-BetaDF <- data.frame(BetaPoints)
-colnames(BetaDF) <- c("Longitude", "Latitude", "Beta")
-
-theme_set(theme_void())
-RawLivBetaMapTile <- ggplot() + geom_tile(data=BetaDF, aes(x=Longitude, y=Latitude, fill=Beta)) + 
-  scale_fill_gradientn(name="β-diversity", colours=cols, na.value="transparent", limits = c(0,1)) + 
-  coord_equal()
-RawLivBetaMapTile
-
-#Add continental and mountainous outlines
+# 2.2 Add continental and mountainous outlines
 nw_mount <- shapefile("Data/MapOutlines/Mountains/Koeppen-Geiger_biomes.shp")
 nw_bound <- shapefile("Data/MapOutlines/Global_bound/Koeppen-Geiger_biomes.shp")
 
 nw_mount_sf <- st_as_sf(nw_mount)
 nw_bound_sf <- st_as_sf(nw_bound)
 
+
+# 2.3 Mapping method from Bryophytes.Rmd
+BetaRaster <- setValues(BlankRas, BetaVec)
+BetaPoints<-rasterToPoints(BetaRaster)
+BetaDF <- data.frame(BetaPoints)
+colnames(BetaDF) <- c("Longitude", "Latitude", "Beta")
+
+theme_set(theme_void())
+RawBetaMap <- ggplot() + geom_tile(data=BetaDF, aes(x=Longitude, y=Latitude, fill=Beta)) + 
+  scale_fill_gradientn(name="β-diversity", colours=cols, na.value="transparent", limits = c(0,1)) + 
+  coord_equal()
+RawBetaMap
+
+
+# 2.4 Map outlier beta values (>0.5)
+OutlierBetaVec <- rep(0, 15038)
+OutlierBetaVec[Cell8]<-Beta8Vec
+OutlierBetaVec[Cell7]<-Beta7Vec
+OutlierBetaVec[OutlierBetaVec==0]<-NA
+OutlierBetaVec[OutlierBetaVec>0.5]<-NA
+
+OutlierBetaRaster <- setValues(BlankRas, OutlierBetaVec)
+OutlierBetaPoints<-rasterToPoints(OutlierBetaRaster)
+OutlierBetaDF <- data.frame(OutlierBetaPoints)
+colnames(OutlierBetaDF) <- c("Longitude", "Latitude", "Beta")
+
+theme_set(theme_void())
+OutlierBetaMap <- gplot(OutlierBetaRaster, maxpixels=15038) +  geom_tile(aes(fill = value)) + 
+  scale_fill_gradientn(colours="black", na.value="transparent", limits = c(0,1)) + theme(legend.position = "none") + 
+  coord_equal() 
+OutlierBetaMap
+
+
+#Map beta diversity with values over 0.5 shown in dark grey (how many values over 0.5?)
+source("Functions/gplot_data.R")
+gplotB<- gplot_data(BetaRaster)
+gplotOutlier<- gplot_data(OutlierBetaRaster)
+
+LiverwortBetaMap <- ggplot() +
+  geom_tile(data = dplyr::filter(gplotOutlier, !is.na(value)), 
+            aes(x = x, y = y), fill = "gray25") +
+  geom_tile(data = gplotB, 
+            aes(x = x, y = y, fill = value)) +
+  scale_fill_gradientn(name = "β diversity", colours=cols, na.value="transparent", limits = c(0,0.5)) +
+  coord_quickmap() + geom_sf(data = nw_bound_sf, size = 0.5, fill=NA) + 
+  geom_sf(data = nw_mount_sf, size = 0.5, alpha=0.1) + theme_void() +
+  theme(legend.text=element_text(size=20), legend.title=element_text(size=32), axis.title = element_blank())
+LiverwortBetaMap
+
+png("Figures/LiverwortBetaMap.png", width = 1000, height = 1000, pointsize = 30)
+LiverwortBetaMap
+dev.off()

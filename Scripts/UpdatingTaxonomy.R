209184f07e6f6442c50b303e89bc60beb367e0fb
Hailey Napier <haileynapier@Haileys-MBP.hsd1.nh.comcast.net>
2020-07-02 13:51:28 -0400

    Minor changes. Checked BIEN taxonomy.

---
+++ Scripts/UpdatingTaxonomy.R
@@ -0,0 +1,18 @@
+#Updating taxonomy
+#Hailey Napier
+#July 2, 2020
+
+#load packages
+library(BIEN)
+library(dplyr)
+
+# 1.0 BIEN package-----------------------------
+#From Family
+#all NAs
+BIENFamilies <- BIEN_taxonomy_family(FamilyNames)
+
+BIENFamAndOrder <- BIENFamilies %>%
+  dplyr::select(c(scrubbed_family, order))
+BIENFamAndOrder <- BIENFamAndOrder[!duplicated(BIENFamAndOrder$scrubbed_family),]
+
+# 2.0 From research----------------------------

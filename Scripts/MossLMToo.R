5b8ca0d213ea16f0e1bdd2f9ab397e5e4c50d0f4
KathrynDawdy <dawdy1@kenyon.edu>
2020-11-30 13:19:40 -0600

    test AICs and log transform LMDF columns

--- Scripts/MossLMToo.R
+++ Scripts/MossLMToo.R
@@ -22,6 +22,7 @@ nw_mount <- shapefile("Data/MapOutlines/Mountains/Koeppen-Geiger_biomes.shp")
 cols <- c("#D8B70A", "#972D15", "#A2A475", "#81A88D", "#02401B",
            "#446455", "#FDD262", "#D3DDDC", "#C7B19C",
            "#798E87", "#C27D38")
+OrderLM_AIC_DF <-readRDS("Data/OrderLM_AIC_DF.rds")
 
 # 1.0 Make LMDF with montane/lowland column ------------------------------------
 # 1.1 First run MossLM.R (as it is on 11/6/2020 !)
@@ -61,6 +62,14 @@ saveRDS(LMDF2, "Data/LMDF2.rds")
 LMDF3 <- LMDF2
 LMDF3$TotalRichness[which(LMDF3$TotalRichness==0)] <- NA
 saveRDS(LMDF3, "Data/LMDF3.rds")
+
+# 1.8 Convert MAT from celsius to Kelvin
+LMDF4 <- LMDF3
+LMDF4$MAT_Kelvin <- (LMDF4$MAT) + 273.15
+
+LMDF4$LogMAT_Kelvin <- log1p(LMDF4$MAT_Kelvin)
+LMDF4$LogMAP <- log1p(LMDF4$MAP)
+LMDF4$LogTotalRich <- log1p(LMDF4$TotalRichness)
 
 
 # 2.0 Playing around to get comfortable + comparing LMDF2 and LMDF3 ------------
@@ -204,12 +213,99 @@ fig2 <- fig2 %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz))
 fig2
 
 
+#4.4 Log(TotalRichness) by log(MAP) by MAT colored by biome --------------------
+fig3 <- plot_ly(LMDF3, x=~log1p(MAP), y=~MAT, z=~log1p(TotalRichness), 
+               type="scatter3d", mode="markers", color=~Biome, colors=cols,
+               marker=list(size=5))
+axx <- list(
+  #backgroundcolor="rgb(200, 200, 230)",
+  #gridcolor="rgb(255,255,255)",
+  #showbackground=TRUE,
+  #zerolinecolor="rgb(255,255,255)",
+  #title = "log(MAP)"
+)
+
+axy <- list(
+  #backgroundcolor="rgb(230, 200,230)",
+  #gridcolor="rgb(255,255,255)",
+  #showbackground=TRUE,
+  #zerolinecolor="rgb(255,255,255)",
+  #title = "MAT"
+)
+
+axz <- list(
+  #backgroundcolor="rgb(230, 230,200)",
+  #gridcolor="rgb(255,255,255)",
+  #showbackground=TRUE,
+  #zerolinecolor="rgb(255,255,255)",
+  #title = "log(Total Richness)"
+)
+#fig <- fig %>% add_trace(x=~LMDF3$MAP, z=lm(LMDF3$TotalRichness~LMDF3$MAT), mode="lines")
+fig3 <- fig3 %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz))
+fig3
+
+# Star map :)
+funcols <- c()
+funfig <- plot_ly(LMDF3, x=~log1p(MAP), y=~MAT, z=~log1p(TotalRichness), 
+                type="scatter3d", mode="markers", color=~Topo, colors=c("#F0CF4C","#F0CF4C"),
+                marker=list(size=5))
+axx <- list(
+  backgroundcolor="rgb(17, 23, 56)",
+  gridcolor="rgb(255,255,255)",
+  showbackground=TRUE,
+  zerolinecolor="rgb(255,255,255)",
+  title = "log(MAP)"
+)
+
+axy <- list(
+  backgroundcolor="rgb(17, 23, 56)",
+  gridcolor="rgb(255,255,255)",
+  showbackground=TRUE,
+  zerolinecolor="rgb(255,255,255)",
+  title = "MAT"
+)
+
+axz <- list(
+  backgroundcolor="rgb(17, 23, 56)",
+  gridcolor="rgb(255,255,255)",
+  showbackground=TRUE,
+  zerolinecolor="rgb(255,255,255)",
+  title = "log(Total Richness)"
+)
+funfig <- funfig %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz))
+funfig
+
+
 # 5.0 AIC ----------------------------------------------------------------------
 testlm <- lm(TotalRichness~MAT, data=LMDF3)
-
 testlm2 <- lm(TotalRichness~MAT + MAP, data=LMDF3)
+testlm3 <- lm(TotalRichness~log1p(MAT), data=LMDF3)
+testlm4 <- lm(TotalRichness~log1p(MAT) + log1p(MAP), data=LMDF3)
+testlm5 <- lm(TotalRichness~MAP, data=LMDF3)
+testlm6 <- lm(TotalRichness~log1p(MAP), data=LMDF3)
+testlm7 <- lm(log1p(TotalRichness)~log1p(MAP), data=LMDF3)
+testlm8 <- lm(log1p(TotalRichness) ~ MAT, data=LMDF3)
+testlm9 <- lm(log1p(TotalRichness) ~ log1p(MAP), data=LMDF3)
+testlm10 <- lm(log1p(TotalRichness) ~ MAT + log1p(MAP), data=LMDF3)
 
 AIC(testlm, testlm2)
+AIC(testlm3)
+AIC(testlm4)
+AIC(testlm3, testlm4)
+AIC(testlm, testlm2, testlm3, testlm4)  #"models are not all fitted to the same number of observations"- probably because NaNs produced when you take the log of negative MATs
+AIC(testlm, testlm3)
+AIC(testlm5, testlm6)
+AIC(testlm5, testlm7)
+AIC(testlm8, testlm9, testlm10)
+
+testDF <- LMDF3
+testDF$LogMAT <- log1p(testDF$MAT)
+testDF$LogMAP <- log1p(testDF$MAP)
+testDF$LogTotalRich <- log1p(testDF$TotalRichness)
+
+lm_fx_test
+
+
 
 
 # ?.? Truly not sure if anything below works ----
